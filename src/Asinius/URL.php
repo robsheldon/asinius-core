4af3fbaf8985b2cfb34a878b6fd2e5bc09b81008
Rob <rob@robsheldon.com>
2020-03-18 17:30:58 -0700

    Add support for imap urls to the core URL lib.

--- src/Asinius/URL.php
+++ src/Asinius/URL.php
@@ -55,7 +55,7 @@ class URL
         'git'                           => '',
         'http'                          => '\Asinius\HTTP\URL',
         'https'                         => '\Asinius\HTTP\URL',
-        'imap'                          => '',
+        'imap'                          => '\Asinius\Imap\URL',
         'irc'                           => '',
         'irc6'                          => '',
         'jabber'                        => '',
