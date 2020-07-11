7dd5845d02a0777488aae577f91bcb72fd433ca8
Rob <rob@robsheldon.com>
2020-07-10 20:08:33 -0700

    Add delimiter stripping to Functions::str_chunk()

--- src/Asinius/Functions.php
+++ src/Asinius/Functions.php
@@ -63,31 +63,33 @@ class Functions
      * by any of those characters, or it can be an array of strings, which will
      * cause $string to be split only by those exact sequences of characters.
      *
-     * Each chunk will begin with the delimiter it was split on.
+     * Each chunk will begin with the delimiter it was split on unless
+     * $strip_delimiter is set to true.
      * 
      * @param   string      $string
      * @param   mixed       $delimiters
      * @param   integer     $limit
      * @param   array       $quotes
+     * @param   boolean     $strip_delimiter
      *
      * @throws  \RuntimeException
      * 
      * @return  array
      */
-    public static function str_chunk ($string, $delimiters, $limit = 0, $quotes = [])
+    public static function str_chunk ($string, $delimiters, $limit = 0, $quotes = [], $strip_delimiter = false)
     {
         $chunks = [];
         //  Start by decomposing the string into quoted and unquoted chunks.
         //  This is best done by building a yucky regex for preg_split().
         if ( ! empty($quotes) ) {
-            $regex_quotes = [];
             if ( ! is_array($quotes) ) {
-                throw new \RuntimeException("\$quotes parameter needs to be an array, " . gettype($quotes) . " given.");
+                throw new \RuntimeException('$quotes parameter needs to be an array, ' . gettype($quotes) . ' given', EINVAL);
             }
+            $regex_quotes = [];
             foreach ($quotes as $quote_delims) {
                 if ( ! is_array($quote_delims) || count($quote_delims) != 2 ) {
                     //  This is an awful error message but the best I can do at the moment.
-                    throw new \RuntimeException("\$quotes parameter needs to be an array of array pairs.");
+                    throw new \RuntimeException('$quotes parameter needs to be an array of array pairs', EINVAL);
                 }
                 list($open, $close) = [preg_quote($quote_delims[0], '/'), preg_quote($quote_delims[1], '/')];
                 $regex_quotes[] = "[$open][^$close]*[$close]";
@@ -100,25 +102,43 @@ class Functions
         }
         //  $quoted_chunks is now an array of some number of quote-enclosed
         //  chunks from $string. The 0th array element will be unquoted, the
-        //  1th will be quoted, the 2th will be unquoted, and so on.
+        //  1st will be quoted, the 2nd will be unquoted, and so on.
         //  Thus every even-indexed array element will be further split by
         //  $delimiters, while the odd-indexed elements will be returned unchanged.
-        if ( is_string($delimiters) ) {
-            $regex = '[' . preg_quote($delimiters, '/') . '][^' . preg_quote($delimiters, '/') . ']*';
-        }
-        else if ( is_array($delimiters) ) {
-            $delimiters = implode('|', array_map(function($delimiter){
-                return preg_quote($delimiter, '/');
-            }, $delimiters));
-            //  Okay, this right here is some nightmare fuel. If I'm luckier
-            //  than I deserve, I'll never have to debug this.
-            $regex = "(?:.(?!{$delimiters}))*(?:(?={$delimiters}).)*.?";
+        $regex = '';
+        if ( $strip_delimiter ) {
+            $flags = PREG_SPLIT_NO_EMPTY;
+            if ( is_string($delimiters) ) {
+                $regex = '/[' . preg_quote($delimiters, '/') . ']/';
+            }
+            else if ( is_array($delimiters) ) {
+                $delimiters = implode('|', array_map(function($delimiter){
+                    return preg_quote($delimiter, '/');
+                }, $delimiters));
+                $regex = "/({$delimiters})/";
+            }
         }
         else {
-            throw new \RuntimeException("\$delimiters parameter needs to be a string or an array, " . gettype($delimiters) . " given.");
+            //  These regular expressions are crafted to include the matching
+            //  delimiter at the start of each token.
+            $flags = PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY;
+            if ( is_string($delimiters) ) {
+                $regex = '/([' . preg_quote($delimiters, '/') . '][^' . preg_quote($delimiters, '/') . ']*)/';
+            }
+            else if ( is_array($delimiters) ) {
+                $delimiters = implode('|', array_map(function($delimiter){
+                    return preg_quote($delimiter, '/');
+                }, $delimiters));
+                //  Okay, this right here is some nightmare fuel. If I'm luckier
+                //  than I deserve, I'll never have to debug this.
+                $regex = "/((?:.(?!{$delimiters}))*(?:(?={$delimiters}).)*.?)/";
+            }
+        }
+        if ( $regex == '' ) {
+            throw new \RuntimeException('$delimiters parameter needs to be a string or an array, ' . gettype($delimiters) . ' given', EINVAL);
         }
         while ( count($quoted_chunks) && ($limit < 1 || count($chunks) < $limit) ) {
-            $unquoted_chunks = preg_split("/($regex)/", array_shift($quoted_chunks), 0, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+            $unquoted_chunks = preg_split($regex, array_shift($quoted_chunks), 0, $flags);
             if ( count($quoted_chunks) ) {
                 //  If there's another quoted chunk remaining, then the last
                 //  element of the unquoted chunk should be a delimiter, which
@@ -128,9 +148,7 @@ class Functions
             $chunks = array_merge($chunks, $unquoted_chunks, [array_shift($quoted_chunks)]);
         }
         //  Remove any pollution in the output from array_pop() or bad preg_split() results.
-        $chunks = array_filter($chunks, function($chunk){
-            return is_string($chunk) && strlen($chunk) > 0;
-        });
+        $chunks = array_filter($chunks, 'strlen');
         //  Finally, ensure that the chunk count didn't exceed $limit; this
         //  can happen pretty easily with preg_split().
         $chunks = array_merge($chunks, $quoted_chunks);
