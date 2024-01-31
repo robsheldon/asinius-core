20132af2a1626c8db8060fa53e61e2334c61d909
Rob <rob@robsheldon.com>
2024-01-30 22:44:03 -0800

    Add `IOBuffer` (not yet complete, but working) and first phpunit tests for it

---
+++ src/Asinius/IOBuffer.php
@@ -0,0 +1,261 @@
+<?php
+
+/*******************************************************************************
+*                                                                              *
+*   Asinius\IOBuffer                                                           *
+*                                                                              *
+*   Store data as a bytestream and read it back as bytes, chars, or lines.     *
+*                                                                              *
+*   Applications can switch read modes at any time (although it is a bit       *
+*   expensive to do so). IOBuffers will store data up to the limits set by     *
+*   the application or caller, so they can be rewound as needed.               *
+*                                                                              *
+*   IOBuffers do not handle any device I/O themselves, but they make it        *
+*   easier for other classes to provide an interface between I/O devices       *
+*   and the application.                                                       *
+*                                                                              *
+*   LICENSE                                                                    *
+*                                                                              *
+*   Copyright (c) 2024 Rob Sheldon <rob@robsheldon.com>                        *
+*                                                                              *
+*   Permission is hereby granted, free of charge, to any person obtaining a    *
+*   copy of this software and associated documentation files (the "Software"), *
+*   to deal in the Software without restriction, including without limitation  *
+*   the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+*   and/or sell copies of the Software, and to permit persons to whom the      *
+*   Software is furnished to do so, subject to the following conditions:       *
+*                                                                              *
+*   The above copyright notice and this permission notice shall be included    *
+*   in all copies or substantial portions of the Software.                     *
+*                                                                              *
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS    *
+*   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                 *
+*   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.     *
+*   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY       *
+*   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,       *
+*   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE          *
+*   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                     *
+*                                                                              *
+*   https://opensource.org/licenses/MIT                                        *
+*                                                                              *
+*******************************************************************************/
+
+namespace Asinius;
+
+class IOBuffer
+{
+
+    //  Read modes.
+    const   RAWMODE       = 0b000000010000000000000000;
+    const   CHARMODE      = 0b000000100000000000000000;
+    const   LINEMODE      = 0b000001000000000000000000;
+    const   MODEMASK      = self::RAWMODE | self::CHARMODE | self::LINEMODE;
+
+    //  Other options.
+    const   TRACKING      = 0b000010000000000000000000;
+
+
+    protected         string $_pending         = '';
+    protected         mixed  $_cache           = '';
+    protected         int    $_cache_position  = 0;
+    protected        ?int    $_max_buffer_size = null;
+    public    static  int    $MAX_BUFFER_SIZE  = PHP_INT_MAX;
+    protected         int    $_flags           = self::RAWMODE;
+    protected        ?string $_charset         = null;
+
+
+    /**
+     * Set the maximum buffer size for this instance. This overrides the static
+     * IOBuffer::$MAX_BUFFER_SIZE value (whether it's larger or smaller).
+     *
+     * @param ?int $size
+     *
+     * @return int
+     */
+    public function max_buffer_size (int $size = null): int
+    {
+        if ( $size !== null ) {
+            $this->_max_buffer_size = $size;
+        }
+        return $this->_max_buffer_size ?? static::$MAX_BUFFER_SIZE;
+    }
+
+
+    /**
+     * Return a new IOBuffer.
+     */
+    public function __construct ()
+    {
+        //  No setup required at this time.
+    }
+
+
+    /**
+     * Append some raw data to this IOBuffer.
+     *
+     * Code that interfaces with an i/o device would use this to append data to
+     * their IOBuffer after reading it from the device.
+     *
+     * Data is immediately converted (as much as possible) into the type expected
+     * by the IOBuffer's read mode.
+     *
+     * @param string $data
+     *
+     * @return void
+     */
+    public function append (string $data): void
+    {
+        //  Process the raw data according to the current read mode option and
+        //  append the result to the cache.
+        if ( $this->_flags & static::RAWMODE ) {
+            $this->_cache .= $data;
+            $cache_size = strlen($this->_cache);
+        }
+        else {
+            $this->_pending .= $data;
+            if ( $this->_flags & static::CHARMODE ) {
+                $chunk = Multibyte::strcut($this->_pending, 0, null, $this->_charset);
+                $this->_pending = substr($this->_pending, strlen($chunk));
+                $this->_cache   = array_merge($this->_cache, Multibyte::str_split($chunk, 1, $this->_charset));
+            }
+            else {
+                //  The end-of-line delimiter MUST be returned, otherwise
+                //  there's no way to make write() and read() idempotetent.
+                //  If a caller write()s the contents of a file read() in
+                //  line mode, the caller can't know if the file was
+                //  terminated in a newline or not.
+                $lines = preg_split("/(\r?\n)/", $this->_pending, 0, PREG_SPLIT_DELIM_CAPTURE);
+                if ( count($lines) > 1 ) {
+                    //  Save the last (possibly incomplete) line in the pending buffer.
+                    $this->_pending = array_pop($lines);
+                    $this->_cache   = array_merge($this->_cache, $lines);
+                }
+            }
+            $cache_size = count($this->_cache);
+        }
+        //  Trim the read cache if needed after appending to it.
+        $trim = min($this->_cache_position, max(0, $cache_size - ($this->_max_buffer_size ?? static::$MAX_BUFFER_SIZE)));
+        if ( $trim > 0 ) {
+            $this->_cache = is_array($this->_cache) ? array_slice($this->_cache, $trim) : substr($this->_cache, $trim);
+            $this->_cache_position -= $trim;
+        }
+    }
+
+
+    /**
+     * Retrieve $count bytes, chars, or lines from the IOBuffer without moving
+     * the internal read index. i.e., two consecutive peek() operations will
+     * return the same data.
+     *
+     * Callers can (should) provide a callback function $read_callback. This
+     * function will be called if peek() needs to return more data than is
+     * currently stored in the IOBuffer, and the callback function should try
+     * to append() more data to this IOBuffer.
+     *
+     * $read_callback will be called as $read_callback(IOBUffer $this, int $needed_count)
+     * NOTE: $needed_count is _not_ guaranteed to be a number of bytes! If the
+     * IOBuffer's read mode is CHARMODE or LINEMODE, $needed_count will be the
+     * number of chars or lines needed, respectively. But, this is okay! The
+     * callback will be called repeatedly until the peek() request is fulfilled
+     * or the callback is not able to append() any more data to the IOBuffer.
+     *
+     * @param int $count
+     * @param callable|null $read_callback
+     *
+     * @return array|string|null
+     */
+    public function peek (int $count = 1, ?Callable $read_callback = null): array|string|null
+    {
+        if ( $count < 0 ) {
+            return null;
+        }
+        $last_cache_size = -1;
+        $cache_is_array = is_array($this->_cache);
+        while ( true ) {
+            //  Estimate how much data is still needed.
+            $cache_size = $cache_is_array ? count($this->_cache) : strlen($this->_cache);
+            $remaining = $count - $cache_size + $this->_cache_position;
+            //  1. Return null if:
+            //      a. No data is available and no data was returned by $read_callback.
+            //  2. Return some or all of the read cache if:
+            //      a. The requested data is already in the cache;
+            //      b. Some data was returned by $read_callback but no more is available;
+            //      c. Data has been returned by $read_calback and it satisfies the request.
+            if ( $last_cache_size ===  $cache_size || $remaining <= 0 ) {
+                if ( $remaining === $count ) {
+                    //  No more data available (at this time?).
+                    //  Signal that peek() failed to retrieve any data.
+                    return null;
+                }
+                //  Return whatever is in the cache.
+                if ( $cache_is_array ) {
+                    $out = array_slice($this->_cache, $this->_cache_position, min($count, $cache_size - $this->_cache_position));
+                    return ( $count === 1 && count($out) > 0 ) ? $out[0] : $out;
+                }
+                return substr($this->_cache, $this->_cache_position, min($count, $cache_size - $this->_cache_position));
+            }
+            if ( $read_callback !== null ) {
+                //  Ask the caller to try append()ing a $remaining amount of data,
+                //  probably through a read() on their i/o device.
+                //  $remaining might not be in bytes! But, this will get called
+                //  repeatedly until the peek() request is fulfilled, so we're okay.
+                $read_callback($this, $remaining);
+            }
+            $last_cache_size = $cache_size;
+        }
+    }
+
+
+    /**
+     * Return up to $count bytes, chars, or lines from the IOBuffer and update
+     * the internal read index.
+     *
+     * Callers can (should) provide a callback function $read_callback. This
+     * function will be called if read() needs to return more data than is
+     * currently stored in the IOBuffer, and the callback function should try
+     * to append() more data to this IOBuffer.
+     *
+     * @param int           $count
+     * @param callable|null $read_callback
+     *
+     * @return array|string|null
+     */
+    public function read (int $count = 1, ?Callable $read_callback = null): array|string|null
+    {
+        $out = $this->peek($count, $read_callback);
+        if ( $out !== null ) {
+            $this->_cache_position += is_string($out) ? strlen($out) : count($out);
+        }
+        return $out;
+    }
+
+
+    /**
+     * Return any unread data, _and_ any data left in the internal "pending"
+     * buffer, in whatever mode the IOBuffer is currently using, and then clear
+     * all buffers and reset counters. This is typically used to get the last
+     * incomplete bit of data (if any) in char or line modes before closing the
+     * device attached to the IOBuffer.
+     *
+     * @return string|array
+     */
+    public function flush (): string|array
+    {
+        if ( is_string($this->_cache) ) {
+            $out = $this->_cache . $this->_pending;
+            $this->_cache = '';
+        }
+        else {
+            if ( $this->_pending != '' ) {
+                $out = array_merge($this->_cache, [$this->_pending]);
+            }
+            else {
+                $out = $this->_cache;
+            }
+            $this->_cache = [];
+        }
+        $this->_pending = '';
+        $this->_cache_position = 0;
+        return $out;
+    }
+}
---
+++ test/unit/IOBufferTest.php
@@ -0,0 +1,94 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Asinius\IOBuffer;
+
+final class IOBufferTest extends TestCase
+{
+
+    /**
+     * Generate random test data of $size bytes.
+     *
+     * @param int $size
+     *
+     * @throws Exception
+     *
+     * @return string
+     */
+    private function generate_random_data (int $size): string
+    {
+        $pool = '';
+        $pool_size = 0;
+        $tries = 0;
+        while (strlen($pool) < $size) {
+            try {
+                $pool .= preg_replace('/[^!-~]/', '', random_bytes(256));
+            }
+            catch (Exception $e) {
+            }
+            if ( strlen($pool) === $pool_size ) {
+                $tries++;
+                if ( $tries > 4 ) {
+                    throw new Exception("Can't add random data to pool: random_bytes() is repeatedly throwing an error");
+                }
+            }
+            else {
+                $tries = 0;
+                $pool_size = strlen($pool);
+            }
+        }
+        return substr($pool, 0, $size);
+    }
+
+
+    /**
+     * Simplest test iteration: immediately append a blob of data into an IOBUffer
+     * and then read it all back out again in one call.
+     *
+     * @throws Exception
+     *
+     * @return void
+     */
+    public function test_raw_read (): void
+    {
+        $data_in = $this->generate_random_data(1024);
+        $buffer = new IOBuffer();
+        $buffer->append($data_in);
+        $data_out = $buffer->read(1024);
+        $this->assertSame($data_in, $data_out);
+        //  Should be no more data available.
+        $this->assertNull($buffer->read(1));
+    }
+
+
+    /**
+     * Append random data one byte at a time to an IOBuffer and read it back again.
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
+    public function test_raw_read_sequential_bytes (): void
+    {
+        //  Read a randomly-generated 1K pool of bytes into the IOBuffer and
+        //  back out again.
+        $data_in = $this->generate_random_data(1024);
+        $data_out = '';
+        $iobuffer = new IOBuffer();
+        for ($i = 0; $i < 1024; $i++) {
+            $byte = $iobuffer->read(1, function($buffer, $count) use (&$i, $data_in){
+                $next = substr($data_in, $i, $count);
+                $buffer->append($next);
+            });
+            if ( $byte === null ) {
+                //  It shouldn't be. But also, don't need to unnecessarily add 1024
+                //  assertions here.
+                $this->assertNotNull($byte);
+            }
+            $data_out .= $byte;
+        }
+        $this->assertSame($data_in, $data_out);
+        $this->assertNull($iobuffer->read(1));
+    }
+
+}
